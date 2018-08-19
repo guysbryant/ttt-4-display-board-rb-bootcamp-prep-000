bdcdea11def736cba3ac164102181160bffbf0c7
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-19 21:34:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  newArr = board.collect |x|
+  newArr = board.collect |x| do
+    if x == "x"
   puts newArr
 end

d0db79fede3ed140df9fde57e6062ee068541745
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-19 21:26:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts board[0]
+  if board.length == 0
+    puts "   |   |   "
+  end
 end
